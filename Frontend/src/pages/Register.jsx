c0160bc63a935ff7ed0556e3485a91d0d8d07520
Gourav Meena <131522059+gouravm2005@users.noreply.github.com>
2025-07-14 20:05:58 +0530

    Update Register.jsx

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -14,7 +14,7 @@ const Register = () => {
  const handleSubmit = async(e) => {
   e.preventDefault();
   try {
-   const res = await axios.post('http://localhost:3000/api/auth/register',  {fullname: {
+   const res = await axios.post(`${import.meta.env.VITE_BASE_URL}/api/auth/register`,  {fullname: {
         firstname: form.firstname,
         lastname: form.lastname,
       },
